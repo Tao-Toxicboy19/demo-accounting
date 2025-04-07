516f728caedb8300f0a00291e691fe4f9fafb926
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-04-07 19:40:12 +0700

    [IMP] sign in with google in firebase, login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Button from '../components/Button';
+import Google from '../assets/google.svg';
+import Icons from '../components/Icons';
+import { signInWithGoogle } from '../services/signInWithGoogle';
+import { JSX } from 'react';
 
-type Props = {}
-
-export default function LoginPage({}: Props) {
+export default function LoginPage(): JSX.Element {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="w-full h-screen flex justify-center items-center">
+      <Button
+        startIcon={<Icons path={Google} className="size-5" />}
+        onClick={signInWithGoogle}
+      >
+        Sign in with Google
+      </Button>
+    </div>
+  );
 }
