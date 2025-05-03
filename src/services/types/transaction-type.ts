f28a5448aed275ad4f2547bb38219978e87f1bc8
thewarat-tao <thewarat.kho@trinityroots.co.th>
2025-05-03 08:06:21 +0700

    [REF] refactor transaction, type,api,hook

--- src/services/types/transaction-type.ts
+++ src/services/types/transaction-type.ts
@@ -1,7 +1,31 @@
-export type Transaction = {
+// export type Transaction = {
+//   _id: string;
+//   user: string;
+//   type: 'income' | 'expense' | 'installment';
+//   title: string;
+//   amount: number;
+//   date: Date;
+//   category: string;
+//   note: string;
+// };
+
+// export type TransactionForm = Omit<Transaction, '_id' | 'user' | 'date'> & {
+//   date: string;
+//   installmentId?: string;
+// };
+
+// export type UserTransactionForm = TransactionForm & {
+//   user: string;
+// };
+
+// export type IdWithUserTransaction = Pick<Transaction, 'user'> & {
+//   id: string;
+// };
+
+export type TransactionEntity = {
   _id: string;
   user: string;
-  type: 'income' | 'expense' | 'installment';
+  type: TransactionType;
   title: string;
   amount: number;
   date: Date;
@@ -9,17 +33,21 @@ export type Transaction = {
   note: string;
 };
 
-export type TransactionForm = Omit<Transaction, '_id' | 'user' | 'date'> & {
+export type TransactionFormInput = Omit<
+  TransactionEntity,
+  '_id' | 'user' | 'date'
+> & {
   date: string;
   installmentId?: string;
 };
 
-export type UserTransactionForm = TransactionForm & {
+export type CreateTransactionPayload = TransactionFormInput & {
   user: string;
 };
 
-export type IdWithUserTransaction = Pick<Transaction, 'user'> & {
+export type TransactionIdentifier = {
   id: string;
+  user: string;
 };
 
 export type TransactionType = 'income' | 'expense' | 'installment';
